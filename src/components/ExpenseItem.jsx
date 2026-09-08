1909f183418f92b06991c9e23badea82f2d9f46e
elmar-unn <elmar.unn@voco.ee>
2026-09-08 12:25:59 +0000

    yl 4. lahendus.

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,19 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  console.log(props);
-  console.log(props.data.date);
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.data.date} />
 
-  return (
-    <div className="expense-item">
-      <div>{props.data.date.toDateString()}</div>
-
-      <div className="expense-item__description">
-        <h2>{props.data.title}</h2>
-        <div className="expense-item__price">{props.data.price}</div>
-      </div>
-    </div>
-  );
+            <div className="expense-item__description">
+                <h2>{props.data.title}</h2>
+                <div className="expense-item__price">
+                    {props.data.price}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ExpenseItem;
